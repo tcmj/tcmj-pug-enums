d6e8b508274d7bcb15a20e84db508c09094efec2
tcmj <tomdeu@gmail.com>
2017-06-06 00:55:23 +0200

    iso-maven-plugin // initial structure of our maven plugin

---
+++ iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/TISO.java
@@ -0,0 +1,18 @@
+package com.tcmj.iso.mvn;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/** tcmj iso generator maven plugin. */
+@Mojo(name = "generate-enum", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class TISO extends AbstractMojo {
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    getLog().info("x x x x x x x x x x x x x x x x x x x x x x x x x x x x x");
+    getLog().info("x   Welcome to tcmj iso enum generator maven plugin!    x");
+    getLog().info("x x x x x x x x x x x x x x x x x x x x x x x x x x x x x");
+  }
+}
