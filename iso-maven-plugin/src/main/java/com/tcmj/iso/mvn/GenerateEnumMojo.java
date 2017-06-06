a4442cd49579ab41dd13d5d96797aa37936a2c7e
tcmj <tomdeu@gmail.com>
2017-06-06 02:58:46 +0200

    iso-maven-plugin // implemented first property and some welcome message

--- iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GenerateEnumMojo.java
+++ iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GenerateEnumMojo.java
@@ -1,18 +1,48 @@
 package com.tcmj.iso.mvn;
 
+import static com.tcmj.iso.mvn.LittleHelper.arrange;
+import static com.tcmj.iso.mvn.LittleHelper.getLine;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
 
 /** tcmj iso generator maven plugin. */
 @Mojo(name = "generate-enum", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class GenerateEnumMojo extends AbstractMojo {
+  private final Log log = getLog();
+
+  /**
+   * property is only for -D usage. In pom.xml you have to use the fieldname ('className' <- case
+   * sensitive) as configuration xml tag!
+   */
+  @Parameter(
+    property = "tcmj.iso.generate.enum.classname",
+    defaultValue = "com.tcmj.generated.MyEnum",
+    required = true
+  )
+  private String className;
+
+  /** Print actual configuration settings and version info of the plugin. */
+  private void displayYoureWelcome() {
+    log.info("PluginContext: " + getPluginContext());
+    log.info(getLine());
+    log.info(arrange("Welcome to the tcmj iso enum generator maven plugin!"));
+    log.info(arrange("ClassName: " + this.className));
+    log.info(arrange("PluginContext: " + getPluginContext().size()));
+    Object project = getPluginContext().get("project");
+    log.info(arrange("Project: " + project.getClass()));
+    Object pluginDescriptor = getPluginContext().get("pluginDescriptor");
+    log.info(arrange("PluginDescriptor: " + pluginDescriptor.getClass()));
+    log.info(getLine());
+  }
+
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    getLog().info("x x x x x x x x x x x x x x x x x x x x x x x x x x x x x");
-    getLog().info("x   Welcome to tcmj iso enum generator maven plugin!    x");
-    getLog().info("x x x x x x x x x x x x x x x x x x x x x x x x x x x x x");
+    displayYoureWelcome();
   }
 }
---
+++ iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/LittleHelper.java
@@ -0,0 +1,17 @@
+package com.tcmj.iso.mvn;
+
+import org.apache.commons.lang3.StringUtils;
+
+/** Common helping functions. */
+public class LittleHelper {
+  private static final String STARS = StringUtils.repeat("* ", 75);
+
+  public static String arrange(String text) {
+    String posedText = StringUtils.rightPad("  " + text, 143);
+    return StringUtils.join("* *".intern(), posedText, "* *".intern());
+  }
+
+  public static String getLine() {
+    return STARS;
+  }
+}
