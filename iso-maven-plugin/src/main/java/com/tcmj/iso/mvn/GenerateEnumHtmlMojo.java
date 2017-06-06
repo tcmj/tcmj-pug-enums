9604f0a4e450e63df04ede1c01ba6b5df43d68a8
tcmj <tomdeu@gmail.com>
2017-06-06 04:23:42 +0200

    iso-maven-plugin // added constant column parameter and sub data columns

--- iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GenerateEnumHtmlMojo.java
+++ iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GenerateEnumHtmlMojo.java
@@ -2,6 +2,7 @@ package com.tcmj.iso.mvn;
 
 import static com.tcmj.iso.mvn.LittleHelper.arrange;
 import static com.tcmj.iso.mvn.LittleHelper.getLine;
+import java.util.Arrays;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
@@ -19,12 +20,36 @@ public class GenerateEnumHtmlMojo extends GeneralEnumMojo {
   )
   private String dataProvider;
 
+  @Parameter(
+    property = "tcmj.iso.generate.enum.tablexpath",
+    defaultValue = "table", //xpath to a record (also to a table possible),
+    required = true
+  )
+  private String tableXpath;
+
+  @Parameter(
+    property = "tcmj.iso.generate.enum.constantcolumn",
+    defaultValue = "1",
+    required = true
+  )
+  private Integer constantColumn;
+
+  @Parameter(property = "tcmj.iso.generate.enum.subdatacolumns")
+  private Integer[] subDataColumns;
+
   /** Print actual configuration settings and version info of the plugin. */
   @Override
   protected void displayYoureWelcome() {
     super.displayYoureWelcome();
     log.info(arrange("Extracts EnumData from a table of a html document (URL)!"));
     log.info(arrange("DataProvider: URLXPathHtmlDataProvider"));
+    log.info(arrange("TableXpath: " + this.tableXpath));
+    log.info(arrange("ConstantColumn: " + this.constantColumn));
+    if(this.subDataColumns==null){
+      log.info(arrange("SubDataColumns: " + this.subDataColumns));
+    }else{
+      log.info(arrange("SubDataColumns: " + Arrays.toString(this.subDataColumns)));
+    }
     log.info(getLine());
   }
 
