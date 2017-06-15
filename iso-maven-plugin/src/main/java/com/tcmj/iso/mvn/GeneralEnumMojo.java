b956961d0d2e73ac5a4882ef8f5d962a98aa4565
tcmj <tomdeu@gmail.com>
2017-06-15 23:03:21 +0200

    iso-maven-plugin // fixed wrong use of maven logger

--- iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GeneralEnumMojo.java
+++ iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GeneralEnumMojo.java
@@ -6,7 +6,6 @@ import java.io.File;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugin.logging.Log;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -14,7 +13,6 @@ import org.apache.maven.plugins.annotations.Parameter;
 /** tcmj iso generator maven plugin base. */
 @Mojo(name = "generate-enum", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
 public class GeneralEnumMojo extends AbstractMojo {
-  protected final Log log = getLog();
 
   @Parameter(
     property = "tcmj.iso.generate.enum.classname",
@@ -35,19 +33,20 @@ public class GeneralEnumMojo extends AbstractMojo {
 
   /** Print actual configuration settings and version info of the plugin. */
   protected void displayYoureWelcome() {
-    log.info("PluginContext: " + getPluginContext());
-    log.info(getLine());
-    log.info(arrange("Welcome to the tcmj iso enum generator maven plugin!"));
-    log.info(arrange("ClassName: " + this.className));
-    log.info(arrange("SourceDirectory: " + this.sourceDirectory));
-    log.info(arrange("URL: " + this.url));
 
-    log.info(arrange("PluginContext.size: " + getPluginContext().size()));
+    getLog().info("PluginContext: " + getPluginContext());
+    getLog().info(getLine());
+    getLog().info(arrange("Welcome to the tcmj iso enum generator maven plugin!"));
+    getLog().info(arrange("ClassName: " + this.className));
+    getLog().info(arrange("SourceDirectory: " + this.sourceDirectory));
+    getLog().info(arrange("URL: " + this.url));
+
+    getLog().info(arrange("PluginContext.size: " + getPluginContext().size()));
     Object project = getPluginContext().get("project");
-    log.info(arrange("Project: " + project.getClass()));
+    getLog().info(arrange("Project: " + project.getClass()));
     Object pluginDescriptor = getPluginContext().get("pluginDescriptor");
-    log.info(arrange("PluginDescriptor: " + pluginDescriptor.getClass()));
-    log.info(getLine());
+    getLog().info(arrange("PluginDescriptor: " + pluginDescriptor.getClass()));
+    getLog().info(getLine());
   }
 
   @Override
