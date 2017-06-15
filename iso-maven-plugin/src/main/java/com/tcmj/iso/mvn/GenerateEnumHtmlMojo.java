40e3f73a4002afdf7f4f23d85644c40a14fa4d13
tcmj <tomdeu@gmail.com>
2017-06-15 23:14:19 +0200

    iso-maven-plugin // first runnable version (but missing file creation yet)

--- iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GenerateEnumHtmlMojo.java
+++ iso-maven-plugin/src/main/java/com/tcmj/iso/mvn/GenerateEnumHtmlMojo.java
@@ -1,5 +1,17 @@
 package com.tcmj.iso.mvn;
 
+import com.tcmj.iso.api.ClassBuilder;
+import com.tcmj.iso.api.DataProvider;
+import com.tcmj.iso.api.EnumExporter;
+import com.tcmj.iso.api.NamingStrategy;
+import com.tcmj.iso.api.SourceFormatter;
+import com.tcmj.iso.builder.ClassBuilderFactory;
+import com.tcmj.iso.builder.NamingStrategyFactory;
+import com.tcmj.iso.builder.SourceFormatterFactory;
+import com.tcmj.iso.datasources.impl.URLXPathHtmlDataProvider;
+import com.tcmj.iso.exporter.EnumExporterFactory;
+import com.tcmj.iso.exporter.impl.ReportingEnumExporter;
+import com.tcmj.iso.generator.Fluent;
 import static com.tcmj.iso.mvn.LittleHelper.arrange;
 import static com.tcmj.iso.mvn.LittleHelper.getLine;
 import java.util.Arrays;
@@ -21,11 +33,11 @@ public class GenerateEnumHtmlMojo extends GeneralEnumMojo {
   private String dataProvider;
 
   @Parameter(
-    property = "tcmj.iso.generate.enum.tablexpath",
-    defaultValue = "table", //xpath to a record (also to a table possible),
+    property = "tcmj.iso.generate.enum.cssselector",
+    defaultValue = "table", //css selector to a record (also to a table possible),
     required = true
   )
-  private String tableXpath;
+  private String tableCssSelector;
 
   @Parameter(
     property = "tcmj.iso.generate.enum.constantcolumn",
@@ -41,20 +53,77 @@ public class GenerateEnumHtmlMojo extends GeneralEnumMojo {
   @Override
   protected void displayYoureWelcome() {
     super.displayYoureWelcome();
-    log.info(arrange("Extracts EnumData from a table of a html document (URL)!"));
-    log.info(arrange("DataProvider: URLXPathHtmlDataProvider"));
-    log.info(arrange("TableXpath: " + this.tableXpath));
-    log.info(arrange("ConstantColumn: " + this.constantColumn));
-    if(this.subDataColumns==null){
-      log.info(arrange("SubDataColumns: " + this.subDataColumns));
-    }else{
-      log.info(arrange("SubDataColumns: " + Arrays.toString(this.subDataColumns)));
+    getLog()
+        .info(
+            arrange(
+                "Extracts EnumData from a table of a html document using a URLXPathHtmlDataProvider!"));
+    getLog().info(arrange("CSS Locator used to locate the table: " + this.tableCssSelector));
+    getLog().info(arrange("Constant column used in Enum: " + this.constantColumn));
+    getLog().info(arrange("SubData columns to include: " + Arrays.toString(this.subDataColumns)));
+    getLog().info(getLine());
+  }
+
+  private DataProvider getMyDataProvider() {
+    int[] subs = null;
+    if (this.subDataColumns != null) {
+
+      Integer[] objectArray = this.subDataColumns;
+      subs = new int[objectArray.length];
+      for (int ctr = 0; ctr < objectArray.length; ctr++) {
+        subs[ctr] = objectArray[ctr].intValue(); // returns int value
+      }
     }
-    log.info(getLine());
+
+    return new URLXPathHtmlDataProvider(
+        this.className,
+        this.url,
+        this.tableCssSelector, //xpath to a record to further (also to a table possible)
+        this.constantColumn, //enum constant column
+        subs //sub columns
+        );
+  }
+
+  private static NamingStrategy getMyNamingStrategy() {
+    NamingStrategy ns1 = NamingStrategyFactory.extractParenthesis();
+    NamingStrategy ns2 = NamingStrategyFactory.removeProhibitedSpecials();
+    NamingStrategy ns3 = NamingStrategyFactory.camelStrict();
+    NamingStrategy ns4 = NamingStrategyFactory.harmonize();
+    return ns1.and(ns2).and(ns3).and(ns4);
+  }
+
+  private static EnumExporter getMyEnumExporter() {
+    EnumExporter exporterA = EnumExporterFactory.getInMemoryCompilingExporter();
+    EnumExporter exporterB = EnumExporterFactory.getReportingEnumExporter();
+    return exporterA.and(
+        exporterB, exporterB.createOptions(ReportingEnumExporter.LogLevel.SYSTEM_OUT.name()));
   }
 
   @Override
   public void execute() throws MojoExecutionException, MojoFailureException {
-    displayYoureWelcome();
+    super.execute();
+
+    try {
+      final DataProvider myDataProvider = getMyDataProvider();
+      getLog().info(arrange("DataProvider: " + myDataProvider));
+
+      final ClassBuilder bestEnumBuilder = ClassBuilderFactory.getBestEnumBuilder();
+      getLog().info(arrange("ClassBuilder: " + bestEnumBuilder));
+
+      final SourceFormatter bestSourceCodeFormatter =
+          SourceFormatterFactory.getBestSourceCodeFormatter();
+      getLog().info(arrange("SourceFormatter: " + bestSourceCodeFormatter));
+
+      Fluent.builder()
+          .fromDataSource(myDataProvider)
+          .usingClassBuilder(bestEnumBuilder)
+          .usingNamingStrategy(getMyNamingStrategy())
+          .format(bestSourceCodeFormatter)
+          .exportWith(getMyEnumExporter())
+          .end();
+
+    } catch (Exception e) {
+      getLog().error("Cannot create your enum! " + e.toString());
+      throw new MojoExecutionException("ExecutionFailure!", e);
+    }
   }
 }
