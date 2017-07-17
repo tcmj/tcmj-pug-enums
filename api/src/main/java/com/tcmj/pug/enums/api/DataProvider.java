90d9d58438782fdfd9fbdb070ec0aabc6914480c
tcmj <tomdeu@gmail.com>
2017-07-18 00:43:20 +0200

    DataProvider javadoc and functional interface annotation

--- api/src/main/java/com/tcmj/pug/enums/api/DataProvider.java
+++ api/src/main/java/com/tcmj/pug/enums/api/DataProvider.java
@@ -3,8 +3,14 @@ package com.tcmj.pug.enums.api;
 import java.util.Objects;
 import com.tcmj.pug.enums.model.EnumData;
 
-/** API to implement a data loader which can provide enum data from any source. */
+/** API to implement a data loader which can provide enum data from any source. 
+ * A DataProvider should used to load tabular data. You should define the main column used for the
+ * constant names and either no subfields or some subfield columns.
+ * A mandatory field can be a address where to load the data from but this is implementation specific.
+ */
+@FunctionalInterface
 public interface DataProvider {
+  /** Main method used to provide a EnumData object loaded with data. */
   EnumData load();
 
   default DataProvider and(DataProvider other) {
