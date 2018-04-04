0744e68f17b728571d0b90a482a6fd2e29649cf7
tcmj <tomdeu@gmail.com>
2018-04-04 18:08:10 +0200

    v1.3.1 NameTypeValue compareTo should return zero if and only if equals returns true

--- api/src/main/java/com/tcmj/pug/enums/model/NameTypeValue.java
+++ api/src/main/java/com/tcmj/pug/enums/model/NameTypeValue.java
@@ -2,14 +2,15 @@ package com.tcmj.pug.enums.model;
 
 import java.io.Serializable;
 import java.util.Arrays;
-import java.util.function.Function;
 import java.util.Objects;
+import java.util.function.Function;
 import java.util.stream.Stream;
 
 /** Holds all data of a single enum constant values. The enum must consist of a constant name and may have sub fields. */
 public class NameTypeValue implements Comparable<NameTypeValue>, Serializable {
 
   private static final long serialVersionUID = 1L;
+  private static final Function<Object, String> QUOTE_OBJECT = (v) -> "\"".concat(String.valueOf(v)).concat("\"");
 
   public final String constantName;
   public final Object[] values;
@@ -18,7 +19,7 @@ public class NameTypeValue implements Comparable<NameTypeValue>, Serializable {
   public String getConstantName() {
     return constantName;
   }
- 
+
   /** Value of sub element. */
   public Object[] getValue() {
     return values;
@@ -33,7 +34,7 @@ public class NameTypeValue implements Comparable<NameTypeValue>, Serializable {
   public static NameTypeValue of(final String constantName, final Object[] value) {
     Objects.requireNonNull(constantName, "Constant name may not be null!");
     Objects.requireNonNull(value, "Object[] value may not be null!");
-      return new NameTypeValue(constantName, value);
+    return new NameTypeValue(constantName, value);
   }
 
   /** Create a immutable instance of NameTypeValue used to hold a enum constant values without having subfields. */
@@ -48,15 +49,28 @@ public class NameTypeValue implements Comparable<NameTypeValue>, Serializable {
 
   @Override
   public int compareTo(NameTypeValue other) {
-    return other==null?-1:this.constantName.compareTo(other.constantName);
+    return other == null ? -1 : this.constantName.compareTo(other.constantName);
   }
- 
 
-   private static final Function<Object, String> QUOTE_OBJECT = (v) -> "\"".concat(String.valueOf(v)).concat("\"");
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof NameTypeValue)) return false;
+    NameTypeValue that = (NameTypeValue) o;
+    return Objects.equals(getConstantName(), that.getConstantName()) &&
+      Arrays.equals(values, that.values);
+  }
+
+  @Override
+  public int hashCode() {
+    int result = Objects.hash(getConstantName());
+    result = 31 * result + Arrays.hashCode(values);
+    return result;
+  }
 
   @Override
   public String toString() {
     final String[] value = Stream.of(this.values).map(QUOTE_OBJECT).toArray(String[]::new);
-    return String.format( "{\"name\":%s,\"values\":%s}", this.constantName, Arrays.toString(value));
+    return String.format("{\"name\":%s,\"values\":%s}", this.constantName, Arrays.toString(value));
   }
 }
--- api/src/test/java/com/tcmj/pug/enums/api/model/NameTypeValueTest.java
+++ api/src/test/java/com/tcmj/pug/enums/api/model/NameTypeValueTest.java
@@ -2,9 +2,11 @@ package com.tcmj.pug.enums.api.model;
 
 import com.tcmj.pug.enums.model.NameTypeValue;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertThat;
 
 /** @author tcmj */
 public class NameTypeValueTest {
@@ -36,6 +38,25 @@ public class NameTypeValueTest {
     assertThat("aa = aa", one.compareTo(one), is(0));
     assertThat("bb = bb", other.compareTo(other), is(0));
     assertThat("bb > aa", other.compareTo(one), is(1));
+  }
+
+  /** compareTo should return zero if and only if equals returns true. */
+  @Test
+  public void testCompareToContract() throws Exception {
+    NameTypeValue a = NameTypeValue.of("aa");
+    NameTypeValue b = NameTypeValue.of("aa");
+    assertThat("a.equals(b) = true", a.equals(b), is(true));
+    assertThat("a.compareTo(b) = 0", a.compareTo(b), is(0));
+  }
+
+  @Test
+  public void testEqualsContract() throws Exception {
+    NameTypeValue a = NameTypeValue.of("aa", new Object[]{"xxx", "yyy"});
+    NameTypeValue b = NameTypeValue.of("aa", new Object[]{"xxx", "yyy"});
+    assertThat("a.equals(b) = true", a.equals(b), is(true));
+    assertThat("b.equals(a) = true", b.equals(a), is(true));
+    assertThat("a.compareTo(b) = 0", a.compareTo(b), is(0));
+    assertThat("b.compareTo(a) = 0", b.compareTo(a), is(0));
   }
 
   /** Test of toString method, of class NameTypeValue. */
