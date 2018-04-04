7ea136def59a852575f0c57a3aa9bb0846f829a4
tcmj <tomdeu@gmail.com>
2018-04-04 17:29:11 +0200

    v1.3.1 GenerateEnumMojo fixed some warnings

--- maven-plugin/src/main/java/com/tcmj/pug/enums/mvn/GenerateEnumMojo.java
+++ maven-plugin/src/main/java/com/tcmj/pug/enums/mvn/GenerateEnumMojo.java
@@ -1,6 +1,11 @@
 package com.tcmj.pug.enums.mvn;
 
-import com.tcmj.pug.enums.api.*;
+import com.tcmj.pug.enums.api.ClassBuilder;
+import com.tcmj.pug.enums.api.DataProvider;
+import com.tcmj.pug.enums.api.EnumExporter;
+import com.tcmj.pug.enums.api.EnumResult;
+import com.tcmj.pug.enums.api.NamingStrategy;
+import com.tcmj.pug.enums.api.SourceFormatter;
 import com.tcmj.pug.enums.api.fluent.Fluent;
 import com.tcmj.pug.enums.api.tools.NamingStrategyFactory;
 import com.tcmj.pug.enums.builder.ClassBuilderFactory;
@@ -11,7 +16,6 @@ import com.tcmj.pug.enums.model.EnumData;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
-import org.apache.maven.plugin.MojoFailureException;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
@@ -21,11 +25,14 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Stream;
 
-import static com.tcmj.pug.enums.mvn.LogFormatter.*;
+import static com.tcmj.pug.enums.mvn.LogFormatter.arrange;
+import static com.tcmj.pug.enums.mvn.LogFormatter.encloseJavaDoc;
+import static com.tcmj.pug.enums.mvn.LogFormatter.getLine;
 
 /**
  * Main Mojo which extracts data from a URL and creates a java enum source file.
@@ -36,27 +43,27 @@ public class GenerateEnumMojo extends AbstractMojo {
 
   /** Mandatory Property which data provider should be used. This class name should be created in the {@link #getDataProvider()} method. */
   @Parameter(property = "com.tcmj.pug.enums.dataprovider", defaultValue = "com.tcmj.pug.enums.datasources.impl.URLXPathHtmlDataProvider")
-  protected String dataProvider;
+  private String dataProvider;
 
   /** Mandatory Property which defines the full class name (including packages). It defaults to 'com.tcmj.generated.MyEnum'. */
   @Parameter(property = "com.tcmj.pug.enums.classname", defaultValue = "com.tcmj.generated.MyEnum", required = true)
-  protected String className;
+  private String className;
 
   /** Mandatory Property which defines the output path to save the generated enum java files. It defaults to mavens 'project.build.sourceDirectory'. */
   @Parameter(property = "com.tcmj.pug.enums.sourcedirectory", defaultValue = "${project.build.sourceDirectory}", required = true)
-  protected File sourceDirectory;
+  private File sourceDirectory;
 
   /** Mandatory Property which defines the location (url) where to load the input data. */
   @Parameter(property = "com.tcmj.pug.enums.url", required = true)
-  protected String url;
+  private String url;
 
   /** Optional Property to override the column names used for the sub fields in the java enum class. */
-  @Parameter(property = "com.tcmj.pug.enums.subfieldnames", required = false)
-  protected String[] subFieldNames;
+  @Parameter(property = "com.tcmj.pug.enums.subfieldnames")
+  private String[] subFieldNames;
 
   /** Optional Property to write some static javadoc lines to our java enum. */
-  @Parameter(property = "com.tcmj.pug.enums.classjavadoc", required = false)
-  protected String[] javadocClassLevel;
+  @Parameter(property = "com.tcmj.pug.enums.classjavadoc")
+  private String[] javadocClassLevel;
 
   /** Css selector to a record (also to a table possible). */
   @Parameter(property = "com.tcmj.pug.enums.cssselector", defaultValue = "table", required = true)
@@ -67,31 +74,31 @@ public class GenerateEnumMojo extends AbstractMojo {
   private Integer constantColumn;
 
   /** Optional possibility to extract further columns and use it as fields in the enum. */
-  @Parameter(property = "com.tcmj.pug.enums.subdatacolumns", required = false)
+  @Parameter(property = "com.tcmj.pug.enums.subdatacolumns")
   private Integer[] subDataColumns;
 
   /** Optional Property NamingStrategy Constants. */
-  @Parameter(property = "com.tcmj.pug.enums.namingstrategy.constants", required = false)
-  protected String[] namingStrategyConstants;
+  @Parameter(property = "com.tcmj.pug.enums.namingstrategy.constants")
+  private String[] namingStrategyConstants;
 
   /** Optional Property NamingStrategy FieldNames. */
-  @Parameter(property = "com.tcmj.pug.enums.namingstrategy.fields", required = false)
-  protected String[] namingStrategyFieldNames;
+  @Parameter(property = "com.tcmj.pug.enums.namingstrategy.fields")
+  private String[] namingStrategyFieldNames;
 
   /** Optional Property to keep the first row of html tables. */
-  @Parameter(property = "com.tcmj.pug.enums.keepfirstrow", required = false)
-  protected Boolean keepFirstRow = Boolean.FALSE;
+  @Parameter(property = "com.tcmj.pug.enums.keepfirstrow")
+  private Boolean keepFirstRow = Boolean.FALSE;
 
   /** Optional possibility to skip specific records. */
-  @Parameter(property = "com.tcmj.pug.enums.skip.values", required = false)
+  @Parameter(property = "com.tcmj.pug.enums.skip.values")
   private String[] valuesToSkip;
   private List<String> lstValuesToSkip = new ArrayList<>();
 
-  protected static <T> boolean isParameterSet(T[] param) {
+  private static <T> boolean isParameterSet(T[] param) {
     return param != null && param.length > 0;
   }
 
-  protected static NamingStrategy invokeMethod(String methodName) throws InvocationTargetException, IllegalAccessException {
+  private static NamingStrategy invokeMethod(String methodName) throws InvocationTargetException, IllegalAccessException {
     for (Method method : NamingStrategyFactory.class.getMethods()) {
       if (methodName.equalsIgnoreCase(method.getName())) { //don't be strict
         return (NamingStrategy) method.invoke(null);
@@ -144,9 +151,7 @@ public class GenerateEnumMojo extends AbstractMojo {
     }
 
     if (isParameterSet(this.valuesToSkip)) {
-      for (String s : this.valuesToSkip) {
-        this.lstValuesToSkip.add(s);
-      }
+      Collections.addAll(this.lstValuesToSkip, this.valuesToSkip);
       getLog().info(arrange("Input values to skip: " + lstValuesToSkip));
     }
   }
@@ -154,15 +159,14 @@ public class GenerateEnumMojo extends AbstractMojo {
   /**
    * Depending if a parameter is set (or not) we use the default strategies or the defined ones.
    */
-  protected NamingStrategy getNamingStrategyConstantNames() {
+  private NamingStrategy getNamingStrategyConstantNames() {
     if (isParameterSet(this.namingStrategyConstants)) {
-      NamingStrategy namingStrategy = resolveNamingStrategies(this.namingStrategyConstants);
-      return namingStrategy;
+      return resolveNamingStrategies(this.namingStrategyConstants);
     }
     return Fluent.getDefaultNamingStrategyConstantNames();
   }
 
-  protected DataProvider getDataProvider() {
+  private DataProvider getDataProvider() {
     if (this.dataProvider != null && !StringUtils.equals(this.dataProvider, "com.tcmj.pug.enums.datasources.impl.URLXPathHtmlDataProvider")) {
       throw new UnsupportedOperationException("NotYetImplemented ! Cannot change data provider class to: " + this.dataProvider);
     }
@@ -180,14 +184,14 @@ public class GenerateEnumMojo extends AbstractMojo {
   /**
    * Depending if a parameter is set (or not) we use the default strategies or the defined ones.
    */
-  protected NamingStrategy getNamingStrategyFieldNames() {
+  private NamingStrategy getNamingStrategyFieldNames() {
     if (isParameterSet(this.namingStrategyFieldNames)) {
       return resolveNamingStrategies(this.namingStrategyFieldNames);
     }
     return Fluent.getDefaultNamingStrategyFieldNames();
   }
 
-  protected NamingStrategy resolveNamingStrategies(String[] namingStrategyConstants) {
+  private NamingStrategy resolveNamingStrategies(String[] namingStrategyConstants) {
     NamingStrategy strategy = value -> value;
     for (String namingStrategy : namingStrategyConstants) {
       try {
@@ -203,7 +207,7 @@ public class GenerateEnumMojo extends AbstractMojo {
 
 
   @Override
-  public void execute() throws MojoExecutionException, MojoFailureException {
+  public void execute() throws MojoExecutionException {
     try {
       displayYoureWelcome();
 
