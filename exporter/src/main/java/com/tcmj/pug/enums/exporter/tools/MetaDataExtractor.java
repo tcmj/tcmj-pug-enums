88fd1210c5bb3822c10d866da5b95ed5c7b3dbfc
tcmj <tomdeu@gmail.com>
2019-03-13 04:51:15 +0100

    v.1.3.5: MetaDataExtractor pimped with regex and unit tests

--- exporter/src/main/java/com/tcmj/pug/enums/exporter/tools/MetaDataExtractor.java
+++ exporter/src/main/java/com/tcmj/pug/enums/exporter/tools/MetaDataExtractor.java
@@ -4,27 +4,38 @@ import com.tcmj.pug.enums.api.EnumResult;
 import com.tcmj.pug.enums.model.EnumData;
 
 import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /** Extracts some data from the enum source needed by some exporter. */
 public class MetaDataExtractor {
 
   /**
+   * Remove all real javadoc comments (/**).
+   */
+  public static String removeJavadocs(String enumSource) {
+    return enumSource.replaceAll("/\\*\\*(?s:(?!\\*/).)*\\*/", "");
+  }
+
+  /**
+   * Remove all slash-star-comments (/*).
+   */
+  public static String removeDocs(String enumSource) {
+    return enumSource.replaceAll("/\\*(?s:(?!\\*/).)*\\*/", "");
+  }
+
+  /**
    * Extract the java package name from a java source class.
-   * @return null if no package found
    */
   public static String getPackageName(String source) {
-    //todo use regex
-    int idxPkg = Objects.requireNonNull(source, "source").indexOf("package");
-    int idxSemi = source.indexOf(';');
-    if (idxPkg >= 0) {
-      boolean invalid = !source.substring(0, idxPkg).trim().equals("");
-      if (idxSemi == -1 || idxPkg > idxSemi || invalid) {
-        return null;
-      } else {
-        return source.substring((idxPkg + 8), idxSemi);
-      }
-    }
-    return null;
+    Objects.requireNonNull(source, "Cannot get a package from a null input!");
+    String harmonized = removeDocs(removeJavadocs(source));
+    return Stream.of(harmonized.split(System.lineSeparator()))
+      .filter(line -> line.trim().startsWith("package"))
+      .map(pckg -> {
+        String tmp = pckg.trim().substring("package ".length()).trim();
+        return tmp.substring(0, tmp.indexOf(";")).trim();
+      }).collect(Collectors.joining());
   }
 
   /** Simple java class name. eg.: MyEnum */
@@ -88,6 +99,10 @@ public class MetaDataExtractor {
 
   public static String getPackageDirectories(String source) {
     String packageName = getPackageName(source);
+    return getPackageDirectoriesIntern(packageName);
+  }
+
+  private static String getPackageDirectoriesIntern(String packageName) {
     return packageName == null ? null : packageName.replace('.', '/').trim();
   }
 
@@ -95,6 +110,6 @@ public class MetaDataExtractor {
   public static String getPackageDirectories(EnumResult enumResult) {
     EnumData enumData = Objects.requireNonNull(enumResult.getData(), "Cannot get EnumData object from EnumResult!");
     String javaPackageName = Objects.requireNonNull(enumData.getPackageName(), "EnumData.getPackageName() of EnumResult!");
-    return javaPackageName.replace('.', '/').trim();
+    return getPackageDirectoriesIntern(javaPackageName);
   }
 }
